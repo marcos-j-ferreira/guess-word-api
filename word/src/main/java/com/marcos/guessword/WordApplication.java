c5d49733f989e592c4c17acab4644c8fe8214d15
Marcos Júnior Lemes Ferreira <marcos.j.lemes.2004@gmail.com>
2025-07-14 09:28:20 -0300

    docs: settings initial of spring

---
+++ word/src/main/java/com/marcos/guessword/WordApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.guessword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordApplication.class, args);
+	}
+
+}
